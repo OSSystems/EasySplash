ecfc739d482da4957c0406ad9cc15eecea4c75eb
Otavio Salvador <otavio@ossystems.com.br>
2020-02-29 22:35:38 -0300

    Create initial code infrastructure for EasySplash rewrite
    The idea is to toy with the GStreamer use as a way to render the boot
    animation. This reduces a lot of platform specific code we have to
    handle and offer nice possibilities such as audio use during the boot
    process.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+// EasySplash - tool for animated splash screens
+// Copyright (C) 2020  O.S. Systems Software LTDA.
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+fn main() -> Result<(), anyhow::Error> {
+    println!("Hello, world!");
+
+    Ok(())
+}
